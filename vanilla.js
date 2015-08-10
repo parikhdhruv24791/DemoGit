ad1f3a6866c93d6b3a3ce0aa4ed277cb08f37186
dhruvparikh24791 <dhruv.ispl@gmail.com>
2015-08-10 12:08:43 +0530

    trsty
    Signed-off-by: dhruvparikh24791 <dhruv.ispl@gmail.com>

---
+++ vanilla.js
@@ -0,0 +1,20 @@
+asdsadsadas
+das
+die('Leadership Quotes.docxasd
+	a
+	die('asd
+		install.inidsa
+		die('d
+			a
+			Leadership Quotes.docxasd
+			sd
+			s
+			das
+			die('Leadership Quotes.docxad
+				sad
+				slideDown('zxzczx
+					czxc
+					zczxc
+					', function() {
+					
+				});')')')')
